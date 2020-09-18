ee2fe99384f18b73be611ae665344b59f50c73b9
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-18 19:08:54 -0300

    Este commit altera a tela inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            string texto = Console.ReadLine();  
+            string novoTexto = texto
+                .Replace("a", "4").Replace("A", "4")
+                .Replace("E", "3").Replace("e", "3")
+                .Replace("I", "1").Replace("i", "1")
+                .Replace("l", "1").Replace("L", "1")
+                .Replace("o", "0").Replace("O", "0")
+                .Replace("t", "7").Replace("T", "7")
+                .Replace("s", "5").Replace("S", "5");
+            Console.WriteLine(novoTexto);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Leet
 Resposta ao exercício "Leet"
+
+
+**Enunciado**
+
+Faça um tradutor simples para a linguagem Leet Speak.
+
+**Como executar**
+
+Abra o projeto e digite:
+
+dotnet run
